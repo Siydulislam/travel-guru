641251a9b82c08c923636a088b677393598b5ed3
Siydulislam <siydulislam333@gmail.com>
2021-01-17 10:47:17 +0600

    fake data created

---
+++ src/fakeData/data.js
@@ -0,0 +1,120 @@
+const locations = [
+    {
+      "id": 1,
+      "name": "Sajek",
+      "image": 'https://i.ibb.co/s57dGQ6/Sajek.png',
+      "description": "Sajek Valley is an emerging tourist spot in Bangladesh situated among the hills of the Kasalong range of mountains in Sajek union, Baghaichhari Upazila in Rangamati District. The valley is 1,476 feet (450 m) above sea level. Sajek valley is known as the Queen of Hills & Roof of Rangamati.",
+      "hotels": [
+        {
+          id: 0,
+          image: 'https://i.ibb.co/qjWd7bR/Apartment-One.png',
+          title: 'Light bright airy stylish apt & safe peaceful stay',
+          type: '4 guests  2 bedrooms  2 bed  2 baths ',
+          type1: 'Wif Air conditioning Kitchen',
+          type2: 'Cancellation fexibility available',
+          star: '4.9 (20)',
+          dollars: 34
+        },
+        {
+          id: 1,
+          image: 'https://i.ibb.co/zQfSsW0/Apartment-Two.png',
+          title: 'Apartment in Lost Panorama',
+          type: '4 guests  2 bedrooms  2 bed  2 baths ',
+          type1: 'Wif Air conditioning Kitchen',
+          type2: 'Cancellation fexibility available',
+          star: '4.8 (10)',
+          dollars: 52
+        },
+        {
+          id: 2,
+          image: 'https://i.ibb.co/yyk5GJN/Lounge.png',
+          title: 'AR Lounge & Pool (r&r + b&b)',
+          type: '4 guests  2 bedrooms  2 bed  2 baths ',
+          type1: 'Wif Air conditioning Kitchen',
+          type2: 'Cancellation fexibility available',
+          star: '4.9 (25)',
+          dollars: 44
+        }
+      ]
+    },
+    {
+      "id": 2,
+      "name": "Sreemangal",
+      "image": 'https://i.ibb.co/QrjJzRW/Sreemongol.png',
+      "description": "It is said the destination Sreemangal (or Srimangal) is destinationd after Sri Das and Mangal Das; two brothers who settled on the banks of the Hail Haor.[2] A copper plate of Raja Marundanath from the 11th century was found in Kalapur. During an excavation at Lamua, an ancient statue of Ananta Narayan was dug out. In 1454, the Nirmai Shiva Bari was built and still stands today.",
+      "hotels": [
+        {
+          id: 0,
+          image: 'https://i.ibb.co/zQfSsW0/Apartment-Two.png',
+          title: 'Apartment in Lost Panorama',
+          type: '4 guests  2 bedrooms  2 bed  2 baths ',
+          type1: 'Wif Air conditioning Kitchen',
+          type2: 'Cancellation fexibility available',
+          star: '4.8 (10)',
+          dollars: 52
+        },
+        {
+          id: 1,
+          image: 'https://i.ibb.co/yyk5GJN/Lounge.png',
+          title: 'AR Lounge & Pool (r&r + b&b)',
+          type: '4 guests  2 bedrooms  2 bed  2 baths ',
+          type1: 'Wif Air conditioning Kitchen',
+          type2: 'Cancellation fexibility available',
+          star: '4.9 (25)',
+          dollars: 44
+        },
+        {
+          id: 2,
+          image: 'https://i.ibb.co/qjWd7bR/Apartment-One.png',
+          title: 'Light bright airy stylish apt & safe peaceful stay',
+          type: '4 guests  2 bedrooms  2 bed  2 baths ',
+          type1: 'Wif Air conditioning Kitchen',
+          type2: 'Cancellation fexibility available',
+          star: '4.9 (20)',
+          dollars: 34
+        }
+      ]
+    },
+    {
+      "id": 3,
+      "name": "Sundarbans",
+      "image": 'https://i.ibb.co/1r8zNZ6/Sundorbon.png',
+      "profile": "https://i.ibb.co/PYscqbL/bfa7be922fb74a04bf45a5db4b723418.jpg",
+      "description": "The Sundarbans is a mangrove area in the delta formed by the confluence of the Ganges, Brahmaputra and Meghna Rivers in the Bay of Bengal. It spans from the Hooghly River in India's state of West Bengal to the Baleswar River in Bangladesh.",
+      "hotels": [
+        {
+          id: 0,
+          image: 'https://i.ibb.co/yyk5GJN/Lounge.png',
+          title: 'AR Lounge & Pool (r&r + b&b)',
+          type: '4 guests  2 bedrooms  2 bed  2 baths ',
+          type1: 'Wif Air conditioning Kitchen',
+          type2: 'Cancellation fexibility available',
+          star: '4.9 (25)',
+          dollars: 44
+        },
+        {
+          id: 1,
+          image: 'https://i.ibb.co/qjWd7bR/Apartment-One.png',
+          title: 'Light bright airy stylish apt & safe peaceful stay',
+          type: '4 guests  2 bedrooms  2 bed  2 baths ',
+          type1: 'Wif Air conditioning Kitchen',
+          type2: 'Cancellation fexibility available',
+          star: '4.9 (20)',
+          dollars: 34
+        },
+        {
+          id: 2,
+          image: 'https://i.ibb.co/zQfSsW0/Apartment-Two.png',
+          title: 'Apartment in Lost Panorama',
+          type: '4 guests  2 bedrooms  2 bed  2 baths ',
+          type1: 'Wif Air conditioning Kitchen',
+          type2: 'Cancellation fexibility available',
+          star: '4.8 (10)',
+          dollars: 52
+        }
+      ]
+    },
+    
+  ]
+  
+  export default locations;
